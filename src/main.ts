c830b34a688491d9b4740476ae696d66d4da4c24
Saurabh Tiwari <saurabhkk0000@gmail.com>
2025-07-22 15:42:05 +0530

    src

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://codekerdos.in', "https://codekerdos-admin.vercel.app", "http://localhost:3001"], // Replace with your frontend URL
+    origin: ['http://localhost:3000', 'https://codekerdos.in', "https://codekerdos-admin.vercel.app", "http://localhost:3001", "https://test-codekerdosh-backend-test-admin.vercel.app/signin"], // Replace with your frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
